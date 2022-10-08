a326094fd28814c326ce4ad74d1e3cba021b145f
Firmanwijaya <firmanwijayadinata90@gmail.com>
2022-10-09 00:36:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+// import DetailPage from "./pages/DetailPage";
+import { Provider } from "react-redux";
+// import store from "./bootstrap/store";
+
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import mySaga from "./bootstrap/sagas";
+import reducers from "./bootstrap/reducers";
+import createSagaMiddleware from "redux-saga";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const sagaMiddleware = createSagaMiddleware();
+const rootReducer = combineReducers({ reducers });
+const stores = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(mySaga);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={stores}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              {/* <Route path="/detail" element={<DetailPage />}></Route> */}
+            </Routes>
+          </BrowserRouter>
+        </Provider>
     </div>
   );
 }
---
+++ src/pages/detailtodos.js
@@ -0,0 +1,13 @@
+import Navigationbar from "../layout/navbar"
+import Footer from "../layout/footer"
+
+function DetailPage() {
+  return (
+    <>
+      <Navigationbar />
+      <Footer />
+    </>
+  );
+}
+
+export default DetailPage;
