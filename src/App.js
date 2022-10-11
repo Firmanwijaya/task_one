52894af20fc6bd7f94a1e5461e6f6e6f2035bd81
Firman Wijaya <106312766+Firmanwijaya@users.noreply.github.com>
2022-10-11 14:16:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Provider store={store}>
           <BrowserRouter>
             <Routes>
-              <Route path="/task_one" element={<HomePage />}></Route>
+              <Route path="/" element={<HomePage />}></Route>
             </Routes>
           </BrowserRouter>
         </Provider>
